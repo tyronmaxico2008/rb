4e3aacc0da525a75bf1c4b9515d14421b5b8420f
tyronmaxico2008 <amitjha211@outlook.com>
2018-02-06 15:51:32 +0530

    Drop down changes done !

--- Scripts/test.js
+++ Scripts/test.js
@@ -1,14 +1,41 @@
 ﻿var myApp = angular.module("myApp", []);
 
+function getFormData(fields)
+{
+    var fd = new FormData();
+
+    for (var f in fields) {
+        if (fields[f] != null && fields[f] != undefined)
+            fd.append(f, fields[f]);
+    }
+
+    return fd;
+}
+
 function tagging($scope, $http) {
-    $scope.rows = [];
+    //$scope.rows = [];
+    $scope.row = { mainTag_id :null, subTag_id :null }
+    $scope.drp_maintag = [];
+    $scope.drp_subtag = [];
     //$scope.busy = false;
 
-
-    $http.get("../service/getMaintagList").then(function (res) {
-        $scope.rows = res.data;
+    $http.post("../service/getMaintagList").then(function (res) {
+        $scope.drp_maintag = res.data;
         //$scope.busy = false;
     });
+
+    $scope.loadSubTag = function () {
+        
+        var jnPost = { maintagid: $scope.row.mainTag_id };
+        
+        $http.post("../service/getSubTagList", getFormData(jnPost), {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).then(function (res) {
+            $scope.drp_subtag = res.data;
+        });
+    }
+
 
 }
 
