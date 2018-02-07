51c435f1519c01ab7425b8165e41709284222d36
tyronmaxico2008 <amitjha211@outlook.com>
2018-02-07 17:59:05 +0530

    Login fetch from ronit

--- Scripts/test.js
+++ Scripts/test.js
@@ -19,6 +19,7 @@ function tagging($scope, $http) {
     $scope.drp_maintag = [];
     $scope.drp_subtag = [];
     $scope.indexfield = [];
+    $scope.showsave = false;
     //$scope.busy = false;
 
     $http.post("../service/getMaintagList").then(function (res) {
@@ -35,20 +36,56 @@ function tagging($scope, $http) {
             headers: { 'Content-Type': undefined }
         }).then(function (res) {
             $scope.drp_subtag = res.data;
+            
         });
     }
 
     $scope.loadIndexfield = function () {
         var jnPost = { subtagid: $scope.row.subTag_id };
+
         $http.post("../service/getIndexFieldList", getFormData(jnPost), {
             transformRequest: angular.identity,
             headers: { 'Content-Type': undefined }
         }).then(function (res) {
             $scope.indexfield = res.data;
+            $scope.showsave = true;
+            
         });
     }
 
 
 }
 
+function login($scope, $http) {
+    $scope.signin = function () {
+        $scope.chk="";
+        var jnPost = { username: $scope.username, password: $scope.password };
+        
+        $http.post("../service/userlogin", getFormData(jnPost), {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).then(function (res) {
+            if (res.data.result == true)
+            {
+                window.location.href = '../Home/Index';
+
+
+            }
+           
+            else {
+
+                alert("Wrong userid pass");
+
+            }
+
+        }).error(function () {
+
+            alert('failed');
+
+        })
+
+    };
+}
+
 myApp.controller("tagging", tagging);
+myApp.controller("login", login);
